6c4bf56f8c1a147208c2bc0c453906d60cc178bf
Bejjanki-Srivardhan <140094178+Bejjanki-Srivardhan@users.noreply.github.com>
2024-08-25 14:00:09 +0530

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,54 @@
+import random
+
+rock = '''
+    _________
+---'   _______)
+      (________)
+      (________)
+      (_______)
+---.__(______)
+'''
+
+paper = '''
+    _______
+---'   ____)_______
+          __________)
+          ____________)
+         __________)
+---._____________)
+'''
+
+scissors = '''
+    _______
+---'   ____)________
+          ___________)
+       ____________)
+      (_______)
+---.__(_____)
+'''
+
+#Write your code below this line 👇
+game_images = [rock, paper, scissors]
+
+user_choice = int(
+  input(
+    "What do you chose? Type 0 for ROCK, 1 for PAPER, 2 for  SCISSORS.\n "))
+
+if user_choice >= 3 or user_choice < 0:
+  print("You typed an invalid number. so YOU LOSE !!!")
+else:
+  print("You chose :-")
+  print(game_images[user_choice])
+computer_choice = random.randint(0, 2)
+
+print("computer chose :-")
+print(game_images[computer_choice])
+
+if user_choice == 0 and computer_choice == 2:
+  print("You win !")
+elif user_choice == 2 and computer_choice == 0:
+  print("You lose !!!!!!")
+elif computer_choice > user_choice:
+  print("You lose !!!!!!!")
+elif computer_choice == user_choice:
+  print("It's an DRAW !")
